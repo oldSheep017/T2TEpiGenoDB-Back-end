dda0ccff658a9fea0ac87fe2991170728ec768ad
yangchunrui <youngcoding@163.com>
2023-06-15 02:35:28 +0800

    feat
    mysql connect.

---
+++ utils/mysql.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2')
+
+const mysqlConnectionConfig = {
+	host: '47.243.4.52',
+	user: 'root',
+	password: 'HDwzk@525',
+	database: 'CGGE',
+}
+
+const sqlConnect = function (sql, params) {
+	return new Promise((resolve, reject) => {
+		const con = mysql.createConnection(mysqlConnectionConfig)
+		con.query(sql, function (err, results, fields) {
+			if (err) {
+				reject(err)
+			}
+			resolve(results)
+		})
+	})
+}
+
+module.exports = sqlConnect
